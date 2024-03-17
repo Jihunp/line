4059630505cd64cfa9c82ddd88fac6b599325470
Edward Jihun Park <92244135+Jihunp@users.noreply.github.com>
2024-03-17 15:20:56 -0700

    tetris board cells

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,37 @@
 
+import styled from "styled-components";
+import BoardCell from "./BoardCell";
 
-const Board = () => {
+
+const BoardContainer = styled.div`
+  margin: 2em auto;
+  display: grid;
+  grid-gap: 2px;
+  width: 45vw;
+  height: 90vw;
+  background: rgb(32, 0, 64);
+  border: 10px solid rgb(32, 0, 64);
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  
+`
+
+
+const Board = ({ board }) => {
+  const boardStyles = {
+    gridTemplateRows: `repeat(${board.size.rows}, 1fr)`,
+    gridTemplateColumns: `repeat(${board.size.columns}, 1fr)`
+  };
+
   return (
-    <h1>Board</h1>
-  )
-}
+    <BoardContainer className="Board" style={boardStyles}>
+      {board.rows.map((row, y) =>
+        row.map((cell, x) => (
+          <BoardCell key={x * board.size.columns + x} cell={cell} />
+        ))
+      )}
+    </BoardContainer>
+  );
+};
 
 export default Board;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,18 +1,20 @@
-// import styled from "styled-components"
-import Board from "./Board"
-import { useBoard } from "../hooks/useBoard"
+import styled from "styled-components";
+import Board from "./Board";
+import {useBoard} from "../hooks/useBoard";
 
-// const TetrisContainer = styled.div`
-//   position: relative;
-// `;
+const TetrisContainer = styled.div`
+  position: relative;
+`;
 
-
-const Tetris = ({ rows, columns, setGameOVer}) => {
+const Tetris = ({rows, columns, setGameOVer}) => {
+  const [board, setBoard] = useBoard({rows, columns});
 
   return (
-    <Board />
-  )
-  
-}
+    <TetrisContainer className="Tetris">
+      <Board board={board} />
+      {/* <GameStats gameStats={gameStats} */}
+    </TetrisContainer>
+  );
+};
 
 export default Tetris;
