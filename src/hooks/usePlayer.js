07dd81af4c76fcfdbd3fa36fbd6de84ea039eec5
Edward Jihun Park <92244135+Jihunp@users.noreply.github.com>
2024-03-18 23:43:13 -0700

    created usePlayer hook

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -5,12 +5,23 @@ import { randomTetromino } from "../componentFunctions/Tetrominoes"
 const buildPlayer = (previous) => {
   let tetrominoes;
 
-  return {
-    collided: false;
-    isFastDrop: 
+  if (previous) {
+    tetrominoes = [...previous.tetrominoes];
+    tetrominoes.unshift(randomTetromino());
+  } else {
+    tetrominoes = Array(5)
+    .fill(0)
+    .map((_) => randomTetromino());
   }
-
-}
+  
+  return {
+    collided: false,
+    isFastDrop: false,
+    position: {row: 0, column: 4},
+    tetrominoes,
+    tetromino: tetrominoes.pop()
+  };
+};
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState(buildPlayer());
